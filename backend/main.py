e65180b216da7797956717dfa22af6abada831ac
chika-ssy <sen.chika12321@gmail.com>
2026-01-13 15:36:33 +0900

    Vercelのリンク追加

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://beer-ohenro-front.vercel.app/map",
     ],
     allow_credentials=True,
     allow_methods=["*"],
